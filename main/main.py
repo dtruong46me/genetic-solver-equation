0c2e28eac53d4ec13131a62876c8240b1e64f8b1
dncq <97528017+dncq@users.noreply.github.com>
2023-10-21 21:52:31 +0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,28 +1,2 @@
-import os
-import sys
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, path)
-
-from input_handling.core.lexer import Lexer
-from input_handling.core.parser_ import Parser
-from input_handling.core.interpreter import Interpreter
-
-while True:
-	try:
-		text = input("calc > ")
-		lexer = Lexer(text)
-		tokens = lexer.generate_tokens()
-		test_tokens = list(tokens)
-		print(test_tokens)
-		
-		# parser = Parser(tokens)
-		# tree = parser.parse()
-		# print(tree)
-		# if not tree: continue
-		# interpreter = Interpreter()
-		# value = interpreter.visit(tree)
-		# print(value)
-	except Exception as e:
-		print(e)
 
